82cd21e48f246c94eccd9c823c65aa407598b6ee
ArisC1 <67166970+Aris-C@users.noreply.github.com>
2021-06-28 20:58:38 +0000

    started it

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-// Author:
+// Author: Aris Carter
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+let buttonDiv;
+let happyButton;
+let sadButton;
+let trainButton;
+
 
 // Global ML Variables
-//let featureExtractor;
+let featureExtractor;
+let classifier;
+let video;
+let happies;
+let sads;
+let isModelReady;
+let isTrainingComplete;
 
 function setup() {
+  canvasDiv = createDiv;
+  canvas = createCanvas(640,480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading please wait...");
+  textP.parent(textDiv);
+  buildButtons();
+  //initialize happy and sad at 0
+  happies = 0;
+  sads = 0;
+  isModelReady = false;
+  isTrainingComplete = false;
+  //load video
+  video = createCapture(VIDEO, videoReady);
+
 
 }
 
@@ -15,7 +44,12 @@ function draw() {
 }
 
 function buildButtons() {
-
+  buttonDiv = createDiv();
+  happyButton = createButton("Happy");
+  happyButton.parent(buttonDiv);
+  happyButton.mousepressed(function(){
+    
+  });
 }
 
 function videoReady() {
